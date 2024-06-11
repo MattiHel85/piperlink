21db5173ec1d014f3f9e0248b9aaa7b7244efe4d
Matt Simpson <70198997+MattiHel85@users.noreply.github.com>
2024-06-11 14:43:53 +0300

    Add Express server setup and comment out zlib functionality

--- src/index.ts
+++ src/index.ts
@@ -2,37 +2,47 @@ import * as fs from 'fs';
 import * as zlib from 'zlib';
 import * as readline from 'readline';
 import * as path from 'path';
+import express from 'express';
 
-const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout,
+// const rl = readline.createInterface({
+//     input: process.stdin,
+//     output: process.stdout,
+// });
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('Hello, World!');
 });
 
-rl.question('Enter file name: ', (inputFileName) => {
-    const desktopFolder = path.join('C:\\Users\\matti\\Desktop');
-    const outputFolder = path.join(desktopFolder, 'Compressed-Files');
+app.listen(3000, () => {
+    console.log('Server is running on port 3000');
+});
 
-    if(!fs.existsSync(inputFileName)){
-        console.log(`${inputFileName} does not exist`);
-        rl.close();
-    }
+// rl.question('Enter file name: ', (inputFileName) => {
+//     const desktopFolder = path.join('C:\\Users\\matti\\Desktop');
+//     const outputFolder = path.join(desktopFolder, 'Compressed-Files');
 
-    if(!fs.existsSync(outputFolder)){
-        fs.mkdirSync(outputFolder);
-    }
+//     if(!fs.existsSync(inputFileName)){
+//         console.log(`${inputFileName} does not exist`);
+//         rl.close();
+//     }
 
-    const outputFileName = `${path.basename(inputFileName, path.extname(inputFileName))}-compressed.gz`;
-    const outputPath = path.join(outputFolder, outputFileName);
+//     if(!fs.existsSync(outputFolder)){
+//         fs.mkdirSync(outputFolder);
+//     }
 
-    const readStream = fs.createReadStream(inputFileName);
-    const writeStream = fs.createWriteStream(outputPath);
+//     const outputFileName = `${path.basename(inputFileName, path.extname(inputFileName))}-compressed.gz`;
+//     const outputPath = path.join(outputFolder, outputFileName);
 
-    const gzip = zlib.createGzip();
+//     const readStream = fs.createReadStream(inputFileName);
+//     const writeStream = fs.createWriteStream(outputPath);
 
-    readStream.pipe(gzip).pipe(writeStream);
+//     const gzip = zlib.createGzip();
 
-    writeStream.on('finish', () => {
-        console.log(`File ${inputFileName} compressed successfully. Compressed file: ${outputPath}`);
-        rl.close();
-    })
-})
+//     readStream.pipe(gzip).pipe(writeStream);
+
+//     writeStream.on('finish', () => {
+//         console.log(`File ${inputFileName} compressed successfully. Compressed file: ${outputPath}`);
+//         rl.close();
+//     })
+// })
