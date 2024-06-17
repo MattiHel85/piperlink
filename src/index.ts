e37e2b890b6975b013b6e823bf23fd70dc650eeb
Matt Simpson <70198997+MattiHel85@users.noreply.github.com>
2024-06-17 15:59:09 +0300

    updated compress logic to include multiple files

--- src/index.ts
+++ src/index.ts
@@ -19,31 +19,49 @@ const rl = readline.createInterface({
 //     console.log('Server is running on port 3000');
 // });
 
-rl.question('Enter file name: ', (inputFileName) => {
-    const desktopFolder = path.join(os.homedir(),'Desktop');
-    const outputFolder = path.join(desktopFolder, 'Compressed-Files');
+rl.question('Enter file names (separated by comma): ', (inputFileNames) => {
+    rl.question('Name your compressed folder (or leave blank): ', (folderName) => {
+        if (!folderName.trim()){
+            const date = new Date();
+            folderName = date.toISOString().slice(0, 10).replace(/-/g, '');
+        }
 
-    if(!fs.existsSync(inputFileName)){
-        console.log(`${inputFileName} does not exist`);
-        rl.close();
-    }
+        const desktopFolder = path.join(os.homedir(),'Desktop');
+        const outputFolder = path.join(desktopFolder, 'PiperLink Files');
+        const fileNames = inputFileNames.split(',').map(name => name.trim());
 
-    if(!fs.existsSync(outputFolder)){
-        fs.mkdirSync(outputFolder);
-    }
+        // if(!fs.existsSync(inputFileName)){
+        //     console.log(`${inputFileName} does not exist`);
+        //     rl.close();
+        // }
 
-    const outputFileName = `${path.basename(inputFileName, path.extname(inputFileName))}-compressed.gz`;
-    const outputPath = path.join(outputFolder, outputFileName);
+        if(!fs.existsSync(outputFolder)){
+            fs.mkdirSync(outputFolder);
+        }
 
-    const readStream = fs.createReadStream(inputFileName);
-    const writeStream = fs.createWriteStream(outputPath);
+        fileNames.forEach((fileName) => {
+            if(!fs.existsSync(fileName)){
+                console.log(`${fileName} does not exist`);
+                rl.close();
+            }
 
-    const gzip = zlib.createGzip();
+            // const outputFileName = `${path.basename(fileName, path.extname(fileName))}-compressed.gz`;
+            const outputFileName = `${path.basename(fileName, path.extname(folderName))}-compressed.gz`;
+            const outputPath = path.join(outputFolder, outputFileName);
 
-    readStream.pipe(gzip).pipe(writeStream);
+            const readStream = fs.createReadStream(fileName);
+            const writeStream = fs.createWriteStream(outputPath);
 
-    writeStream.on('finish', () => {
-        console.log(`File ${inputFileName} compressed successfully. Compressed file: ${outputPath}`);
+            const gzip = zlib.createGzip();
+
+            readStream.pipe(gzip).pipe(writeStream);
+
+            writeStream.on('finish', () => {
+                console.log(`File ${fileName} compressed successfully. Compressed file: ${outputPath}`);
+                // rl.close();
+            })
+        })
+
         rl.close();
     })
 })
